324ab88a816fc046c1e37434908a30d320177b94
Ryan Strawser <strawser.ryan@gmail.com>
2022-10-14 15:44:13 -0400

    add custom ui to playground

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,8 +2,196 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Example from './example'
+import CustomCard from './CustomCard'
+
+import { useState } from 'react';
+import {
+    Block,
+    Card,
+    ColGrid,
+    Tab,
+    TabList,
+    Text,
+    Title,
+    BadgeDelta,
+    DeltaType,
+    Flex,
+    Metric,
+    ProgressBar,
+
+    Table,
+    TableHead,
+    TableHeaderCell,
+    TableBody,
+    TableRow,
+    TableCell,
+    Badge,
+    Icon
+} from "@tremor/react";
+
+type Kpi = {
+  title: string,
+  metric: string,
+  progress: number,
+  target: string,
+  delta: string,
+  deltaType: DeltaType,
+}
+
+const kpiData: Kpi[] = [
+  {
+      title: 'Sales',
+      metric: '$ 12,699',
+      progress: 15.9,
+      target: '$ 80,000',
+      delta: '13.2%',
+      deltaType: 'moderateIncrease',
+  },
+  {
+      title: 'Profit',
+      metric: '$ 45,564',
+      progress: 36.5,
+      target: '$ 125,000',
+      delta: '23.9%',
+      deltaType: 'increase',
+  },
+  {
+      title: 'Customers',
+      metric: '1,072',
+      progress: 53.6,
+      target: '2,000',
+      delta: '10.1%',
+      deltaType: 'moderateDecrease',
+  }
+];
 
 const Home: NextPage = () => {
+
+  const data = [
+    {
+      name: "Viola Amherd",
+      Role: "Federal Councillor",
+      departement:
+        "The Federal Department of Defence, Civil Protection and Sport (DDPS)",
+      status: "active",
+    },
+    {
+      name: "Simonetta Sommaruga",
+      Role: "Federal Councillor",
+      departement:
+        "The Federal Department of the Environment, Transport, Energy and Communications (DETEC)",
+      status: "active",
+    },
+    {
+      name: "Alain Berset",
+      Role: "Federal Councillor",
+      departement: "The Federal Department of Home Affairs (FDHA)",
+      status: "active",
+    },
+    {
+      name: "Ignazio Cassis",
+      Role: "Federal Councillor",
+      departement: "The Federal Department of Foreign Affairs (FDFA)",
+      status: "active",
+    },
+    {
+      name: "Ueli Maurer",
+      Role: "Federal Councillor",
+      departement: "The Federal Department of Finance (FDF)",
+      status: "active",
+    },
+    {
+      name: "Guy Parmelin",
+      Role: "Federal Councillor",
+      departement:
+        "The Federal Department of Economic Affairs, Education and Research (EAER)",
+      status: "active",
+    },
+    {
+      name: "Karin Keller-Sutter",
+      Role: "Federal Councillor",
+      departement: "The Federal Department of Justice and Police (FDJP)",
+      status: "active",
+    },
+  ];
+
+  // render a tremor card
+  const renderCard = (title: string, content: string) => {
+    return (
+      <Card>
+        <Block>
+          <Title>{title}</Title>
+          <Text>{content}</Text>
+        </Block>
+      </Card>
+    );
+  }
+
+  const renderTable = () => {
+    return (
+      <Card>
+    <Title>List of Swiss Federal Councillours</Title>
+    <Table marginTop="mt-5">
+      <TableHead>
+        <TableRow>
+          <TableHeaderCell>Name</TableHeaderCell>
+          <TableHeaderCell>Position</TableHeaderCell>
+          <TableHeaderCell>Department</TableHeaderCell>
+          <TableHeaderCell>Status</TableHeaderCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((item) => (
+          <TableRow>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>
+              <Text>{item.Role}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{item.departement}</Text>
+            </TableCell>
+            <TableCell>
+              <Badge
+                text={item.status}
+                color="emerald"
+                Icon={Icon.StatusOnlineIcon}
+              />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Card>
+    );
+        }
+
+  const renderKpi = () => {
+    return (
+    <ColGrid numColsMd={ 2 } numColsLg={ 3 } marginTop="mt-6" gapX="gap-x-6" gapY="gap-y-6">
+            { kpiData.map((item) => (
+                <Card key={ item.title }>
+                    <Flex alignItems="items-start">
+                        <Block truncate={ true }>
+                            <Text>{ item.title }</Text>
+                            <Metric truncate={ true }>{ item.metric }</Metric>
+                        </Block>
+                        <BadgeDelta deltaType={ item.deltaType } text={ item.delta } />
+                    </Flex>
+                    <Flex marginTop="mt-4" spaceX="space-x-2">
+                        <Text truncate={ true }>{ `${item.progress}% (${item.metric})` }</Text>
+                        <Text>{ item.target }</Text>
+                    </Flex>
+                    <ProgressBar percentageValue={ item.progress } marginTop="mt-2" />
+                </Card>
+            )) }
+        </ColGrid>
+    )
+  }
+
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +199,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {renderCard("Title", "Content")}
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -21,8 +211,13 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+        {renderKpi()}
+        {renderTable()}
+        {renderCard("Title", "Content")}
 
         <div className={styles.grid}>
+          { /* render a tremor card */ }
+          {renderCard("Title", "Content")}
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -50,6 +245,7 @@ const Home: NextPage = () => {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
+          {renderTable()}
         </div>
       </main>
 
