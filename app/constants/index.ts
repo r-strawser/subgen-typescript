9a6366ed27298f145bf1cd3a916c0732f9c2b95c
Ryan Strawser <strawser.ryan@gmail.com>
2022-10-15 14:07:47 -0400

    add table + data fetching

---
+++ app/constants/index.ts
@@ -0,0 +1,49 @@
+export const FACTORY_CONTRACT_ADDRESS =
+  "0xA03e6e0ACd9C6Ee4403df08B1F4B2a4eccDf72a9";
+  export const abi = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "contractAddress",
+				"type": "address"
+			}
+		],
+		"name": "ContractAdded",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "nftContract",
+				"type": "address"
+			}
+		],
+		"name": "addContract",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "contracts",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
